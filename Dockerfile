b227d7d52a27648389027a2174f5fd6c22e43e63
elisheval <elisheva2272@gmail.com>
2023-08-23 12:21:26 +0300

    register completed

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,14 @@ FROM python:3.8
 # set the working directory in the container
 WORKDIR /code
 
+
 # copy the dependencies file to the working directory
 COPY requirements.txt .
 
 # install dependencies
 RUN pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org -r requirements.txt
 
-
 # copy the content of the local src directory to the working directory
 COPY . .
-
 # command to run on container start
 CMD [ "python", "./chatApp.py" ]
--- chatApp.py
+++ chatApp.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import csv
+from enum import Enum
+import base64
+
+class user_status(Enum):
+    PASS_AND_NAME_MATCH = 1
+    NAME_MATCH = 2
+    NO_MATCH = 3
+    ERROR = 4
+
 
 app = Flask("__name__")
 
+def encode_password(user_pass):
+    pass_bytes = user_pass.encode('ascii')
+    base64_bytes = base64.b64encode(pass_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+def decode_password(user_pass):
+    base64_bytes = user_pass.encode('ascii')
+    pass_bytes = base64.b64decode(base64_bytes)
+    user_pass = pass_bytes.decode('ascii')
+    return user_pass
+
 def add_user_to_csv(username, userpass):
-    with open('users.csv', 'w') as users:
-        writer = csv.writer(users)    
-        writer.writerow("tt")
+    f = open('users.csv', 'a')
+    writer = csv.writer(f)
+    writer.writerow([username,userpass])
+    f.close()
 
 def check_if_user_exists(username, userpass):
     with open('users.csv', 'r') as users: 
@@ -15,32 +37,37 @@ def check_if_user_exists(username, userpass):
             if user[0] == username:
                 if user[1] != userpass:
                      msg = "user with that name already exist"
+                     status = 1
                 else:
                      msg = "you already registered, please login"
-                return True, msg 
-        return False, " "
-    return False, "ERROR"
+                     status = 2
+                return status, msg
+        return 3, " "
+    return 4, "ERROR"
 
-@app.route('/', methods=['GET','POST'])
-def register():
-    users_arr =str(csv.reader(open('users.csv', 'r')))
-    for row in users_arr:
-        return(row[0])
-    # return render_template('register.html')
     
-@app.route('/homePage', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def homePage():
+    msg = " "
     if request.method == 'POST':
         username = request.form['username']
         userpass = request.form['password']
+        status, msg = check_if_user_exists(username, userpass)
+        if status == user_status.NO_MATCH.value :
+            add_user_to_csv(username, userpass)
+            return redirect('/login')
+        elif status == user_status.NAME_MATCH.value:
+           return render_template("login.html")
+    return render_template("register.html")
+@app.route('/login', methods=['GET','POST'])
+def loginPage():
+   if request.method == 'POST':
+        username = request.form['username']
+        userpass = request.form['password']
         user_exists, msg = check_if_user_exists(username, userpass)
-        if not user_exists:
-           add_user_to_csv(username, userpass)
-           return "kok"
-           #    ניתוב ללוגין
-    return "פפפ"
-
-
-
+        if user_exists:
+            return render_template('login.html')
+   return render_template('login.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    app.debug = True
