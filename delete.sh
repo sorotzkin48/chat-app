83b81778f6bd8a0717348547e452bcddf1fc6ecd
sorotzkin48 <chayashpitzer@gmail.com>
2023-08-27 14:28:39 +0300

    comments added

--- delete.sh
+++ delete.sh
@@ -1,4 +1,8 @@
-
+# delete all images
 docker rmi -f $(docker images -aq)
+
+# delete all containers
 docker rm -f $(docker ps -a -q)
+
+#delete volume
 docker volume rm chat-app-data
--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+# create volume
 docker volume create chat-app-data 
+
+# build  images
 docker build -t chat_img:1 .
+
+# run image
 docker run -v chat-app-data:/code -p 5000:5000 chat_img:1
