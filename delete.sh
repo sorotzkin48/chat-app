dd539b06e7c6b8431478930a134f743472e8bf3d
elisheval <elisheva2272@gmail.com>
2023-08-30 10:21:03 +0300

    resolving conflict

--- delete.sh
+++ delete.sh
@@ -3,10 +3,6 @@ docker rmi -f $(docker images -aq)
 
 # delete all containers
 docker rm -f $(docker ps -a -q)
-<<<<<<< HEAD
-docker volume rm chat-app-data
-=======
 
 #delete volume
 docker volume rm chat-app-data
->>>>>>> b83ee59cf6374b19ceecddd428313a5757e84e57
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+check_valid() {
+    # Validate tag name using regex
+    regex_pattern="^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$"
+
+    if ! [[ $1 =~ $regex_pattern ]]; then
+        return 1  # Return a non-zero value to indicate failure
+    fi
+    return 0  # Return 0 to indicate success
+}
+
+read -p "Enter version: " version
+read -p "Enter commit hash: " commit_hash
+
+if check_valid "$version"; then
+    git tag "$version" "$commit_hash"
+    git push origin "$version"
+    docker build -t "chat-app:$version" .
+else
+    echo "Invalid tag name format. Please use semantic versioning (e.g., v1.0.0)!"
+    exit 1
+fi
