d3f0c9a5f7361a0c6764e87b6b46bbd4a5432b27
elisheval <elisheva2272@gmail.com>
2023-08-31 01:07:42 +0300

    init with version

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
 # create volume
 docker volume create chat-app-data 
 
+read -p "Enter version you want to run " version
+
 # build  images
-docker build -t chat_img:1 -f Dockerfile . 
+docker build -t chat_img:${version} -f Dockerfile . 
 
 # run image
-docker run -v chat-app-data:/code  -p 5000:5000 chat_img:1
-
-
- 
+docker run -d -v chat-app-data:/code --name chat-app --memory=256m --cpus=1  -p 5000:5000 chat_img:${version}
