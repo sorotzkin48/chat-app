a4a5baa59a3ba965f74c47b18c50540a74331ffb
sorotzkin48 <chayashpitzer@gmail.com>
2023-09-18 20:03:13 +0300

    modified init & delete

--- delete.sh
+++ delete.sh
@@ -1,8 +1,16 @@
-# delete all images
-docker rmi -f $(docker images -aq)
+#added
+# Stop and remove the running container
+docker stop chat-app
+docker rm chat-app
+# Delete the image
+docker rmi chat_img:${version}
 
-# delete all containers
-docker rm -f $(docker ps -a -q)
+
+# # delete all images
+# docker rmi -f $(docker images -aq)
+
+# # delete all containers
+# docker rm -f $(docker ps -a -q)
 
 #delete volume
 docker volume rm chat-app-data
--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
 # create volume
 docker volume create chat-app-data 
 
-read -p "Enter version you want to run " version
+#changed
+gcloud compute ssh [INSTANCE_NAME] --zone [INSTANCE_ZONE]
+gcloud auth configure-docker
+docker pull chat_img:${version}
+#docker run chat_img:${version}
 
-# build  images
-docker build -t chat_img:${version} -f Dockerfile . 
 
-# run image
-docker run -v chat-app-data:/code/$ROOMS_DIR -v chat-app-data:/code/users --name chat-app --memory=256m --cpus=1  -p 5000:5000 chat_img:${version}
+# read -p "Enter version you want to run " version
+
+# # build  images
+# docker build -t chat_img:${version} -f Dockerfile . 
+
+# # run image
+ docker run -v chat-app-data:/code/$ROOMS_DIR -v chat-app-data:/code/users --name chat-app --memory=256m --cpus=1  -p 5000:5000 chat_img:${version}
